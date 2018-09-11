b443481c3608c819cad635dff3cf44c5a4dc9a05
Kayler C <kmcookhorn@gmail.com>
2018-09-11 12:39:41 -0400

    updated index loop

--- db/migrate/20180905214230_create_cohorts_students.rb
+++ db/migrate/20180905214230_create_cohorts_students.rb
@@ -1,6 +1,6 @@
 class CreateCohortsStudents < ActiveRecord::Migration[5.2]
   def change
-    create_table :cohorts_students, :id => false do |t|
+    create_table :cohorts_students, :id => true do |t|
       t.integer :cohort_id
       t.integer :student_id
     end
