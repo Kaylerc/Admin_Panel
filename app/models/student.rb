cd49597ad3f1202d460bb03cf234a656d1579227
Kayler C <kmcookhorn@gmail.com>
2018-10-01 10:25:59 -0400

    heroku student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ApplicationRecord
   # has_one_attached :profile_photo
   has_many :cohorts, dependent: :destroy
-  has_many :courses, through: :cohorts, dependent: :destroy
+  has_many :courses, through: :cohorts, optional: true, dependent: :destroy 
     # validates :first_name, :last_name, :age, :education, presence: true
     def full_name
       self.first_name + " " + self.last_name
