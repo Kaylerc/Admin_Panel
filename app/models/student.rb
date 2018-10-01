1810d983bd383eb6622605744efb0383c2bea34d
Kayler C <kmcookhorn@gmail.com>
2018-10-01 11:43:55 -0400

    students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ApplicationRecord
   # has_one_attached :profile_photo
   has_many :cohorts, dependent: :destroy
-  has_many :courses, through: :cohorts, dependent: :destroy 
+  has_many :courses, dependent: :destroy
     # validates :first_name, :last_name, :age, :education, presence: true
     def full_name
       self.first_name + " " + self.last_name
@@ -10,3 +10,4 @@ class Student < ApplicationRecord
 end
 
   # belongs_to :cohort
+# through: :cohorts,
